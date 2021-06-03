bdaef0578bf0e14bb308c0e18d781e17c3d85ee4
Stephen Thompson <s.thompson@ucl.ac.uk>
2021-06-03 18:14:22 +0100

    Turning candles on and off

--- static/happybirthday.js
+++ static/happybirthday.js
@@ -6,16 +6,7 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-var candle0on = true 
-var candle1on = true 
-var candle2on = true 
-var candle3on = true 
-var candle4on = true 
-var candle5on = true 
-var candle6on = true 
-var candle7on = true 
-var candle8on = true
-
+var candleson = [ true, true, true, true, true, true, true, true, true ];
 var threshold = 0.1
 
 function get_voltage(index) {
@@ -36,22 +27,37 @@ function get_voltage(index) {
 		var htmlel = 0
 		if (index == 0)
 		  htmlel = document.getElementById('candle0')
+		  image = document.getElementById('candle0img')
 		if (index == 1)
 		  htmlel = document.getElementById('candle1')
+		  image = document.getElementById('candle1img')
 		if (index == 2)
 		  htmlel = document.getElementById('candle2')
+		  image = document.getElementById('candle2img')
 		if (index == 3)
 		  htmlel = document.getElementById('candle3')
+		  image = document.getElementById('candle3img')
 		if (index == 4)
 		  htmlel = document.getElementById('candle4')
+		  image = document.getElementById('candle4img')
 		if (index == 5)
 		  htmlel = document.getElementById('candle5')
+		  image = document.getElementById('candle5img')
 		if (index == 6)
 		  htmlel = document.getElementById('candle6')
+		  image = document.getElementById('candle6img')
 		if (index == 7)
 		  htmlel = document.getElementById('candle7')
+		  image = document.getElementById('candle7img')
 		if (index == 8)
 		  htmlel = document.getElementById('candle8')
+		  image = document.getElementById('candle8img')
+		if ( voltage > threshold )
+		  {
+			  candleson[index] = false;
+			  image.style.display = "none";
+		  }
+
 //		console.log("voltage = ", voltage, htmlel)
 		htmlel.innerHTML = voltage
 	  });
@@ -63,40 +69,85 @@ function get_voltage(index) {
 
 document.addEventListener("keydown", event => {
 	if (event.keyCode == 49) {
-		candle0on = ! candle0on
-        	console.log('Candle 0 is ' , candle0on)
+		candleson[0] = ! candleson[0]
+        	console.log('Candle 0 is ' , candleson[0])
+		image = document.getElementById('candle0img')
+		if (candleson[0])
+			image.style.display ="block";
+		else
+			image.style.display ="none";
 	};
 	if (event.keyCode == 50) {
-		candle1on = ! candle1on
-        	console.log('Candle 1 is ' , candle1on)
+		candleson[1] = ! candleson[1]
+        	console.log('Candle 1 is ' , candleson[1])
+		image = document.getElementById('candle1img')
+		if (candleson[1])
+			image.style.display ="block";
+		else
+			image.style.display ="none";
 	};
 	if (event.keyCode == 51) {
-		candle2on = ! candle2on
-        	console.log('Candle 2 is ' , candle2on)
+		candleson[2] = ! candleson[2]
+        	console.log('Candle 2 is ' , candleson[2])
+		image = document.getElementById('candle2img')
+		if (candleson[2])
+			image.style.display ="block";
+		else
+			image.style.display ="none";
 	};
 	if (event.keyCode == 52) {
-		candle3on = ! candle3on
-        	console.log('Candle 3 is ' , candle3on)
+		candleson[3] = ! candleson[3]
+        	console.log('Candle 3 is ' , candleson[3])
+		image = document.getElementById('candle3img')
+		if (candleson[3])
+			image.style.display ="block";
+		else
+			image.style.display ="none";
 	};
 	if (event.keyCode == 53) {
-		candle4on = ! candle4on
-        	console.log('Candle 4 is ' , candle4on)
+		candleson[4] = ! candleson[4]
+        	console.log('Candle 4 is ' , candleson[4])
+		image = document.getElementById('candle4img')
+		if (candleson[4])
+			image.style.display ="block";
+		else
+			image.style.display ="none";
 	};
 	if (event.keyCode == 54) {
-		candle5on = ! candle5on
-        	console.log('Candle 5 is ' , candle5on)
+		candleson[5] = ! candleson[5]
+        	console.log('Candle 5 is ' , candleson[5])
+		image = document.getElementById('candle5img')
+		if (candleson[5])
+			image.style.display ="block";
+		else
+			image.style.display ="none";
 	};
 	if (event.keyCode == 55) {
-		candle6on = ! candle6on
-        	console.log('Candle 6 is ' , candle6on)
+		candleson[6] = ! candleson[6]
+        	console.log('Candle 6 is ' , candleson[6])
+		image = document.getElementById('candle6img')
+		if (candleson[6])
+			image.style.display ="block";
+		else
+			image.style.display ="none";
 	};
 	if (event.keyCode == 56) {
-		candle7on = ! candle7on
-        	console.log('Candle 7 is ' , candle7on)
+		candleson[7] = ! candleson[7]
+        	console.log('Candle 7 is ' , candleson[7])
+		image = document.getElementById('candle7img')
+		if (candleson[7])
+			image.style.display ="block";
+		else
+			image.style.display ="none";
 	};
 	if (event.keyCode == 57) {
-		candle8on = ! candle8on
-        	console.log('Candle 8 is ' , candle8on)
+		candleson[8] = ! candleson[8]
+        	console.log('Candle 8 is ' , candleson[8])
+		image = document.getElementById('candle8img')
+		if (candleson[8])
+			image.style.display ="block";
+		else
+			image.style.display ="none";
 	};
 });
 
@@ -104,22 +155,24 @@ async function update_voltages(){
 	//console.log("Updating voltage")
 		while (true){
 		  	htmlel = document.getElementById('candle0img')
-			htmlel.style.display ="block"; 
+			//htmlel.style.display ="block"; 
 			console.log('showing' , htmlel)
 			var i;
 			for (i = 0; i < 10 ; i++){
 				var candle;
 				for (candle = 0 ; candle < 9 ; candle++){
-					get_voltage(candle);
+					if (candleson[candle])
+						get_voltage(candle);
 				}
 				await sleep(0400);
 			}
 			console.log('hiding' , htmlel)
-			htmlel.style.display = "none";
+			//htmlel.style.display = "none";
 			for (i = 0; i < 10 ; i++){
 				var candle;
 				for (candle = 0 ; candle < 9 ; candle++){
-					get_voltage(candle);
+					if (candleson[candle])
+						get_voltage(candle);
 				}
 				await sleep(0400);
 			}
